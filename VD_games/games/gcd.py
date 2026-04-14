c02a3a30345e7ef184534024a9049857c9348c20
elina177 <good.56ueue@bk.ru>
2026-04-14 10:52:00 +0300

    Update gcd.py

--- VD_games/games/gcd.py
+++ VD_games/games/gcd.py
@@ -6,14 +6,12 @@ GAME_RULE = 'Find the greatest common divisor of given numbers.'
 
 
 def find_gcd(a, b):
-    """Находит НОД двух чисел"""
     while b != 0:
         a, b = b, a % b
     return a
 
 
 def generate_round():
-    """Возвращает вопрос и правильный ответ для одного раунда"""
     num1 = random.randint(1, 100)
     num2 = random.randint(1, 100)
     question = f"{num1} {num2}"
